5ac1d2590749c6cdfa23bf483d9bdd0c3318d814
Rose Mullan <rfmullan@gmail.com>
2025-02-12 12:25:14 +0000

    invoke seeding in test file

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,4 +1,10 @@
-const db = require('../db/connection')
+const db = require('../db/connection');
+const seed = require('../db/seed/seed');
+const data = require('../db/data/test-data')
+
+beforeEach(() => { 
+    seed(data)
+})
 
 describe("We will be using this file in the very near future", () => {
   test("You'll love it!", () => {});
