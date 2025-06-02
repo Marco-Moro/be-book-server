c5540c8ff25c6c265c0bc7df9f01a12e9e03ed13
Marco-moro <marcomoronese1991@gmail.com>
2025-06-02 18:46:39 +0100

    task 5

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const http = require("http");
+const db = require("./db/connection");
 
 const server = http.createServer((request, response) => {
   const { method, url } = request;
@@ -8,8 +9,15 @@ const server = http.createServer((request, response) => {
     response.write(JSON.stringify({ message: "Hello!" }));
     response.end();
   }
-});
-
+  if (method === "GET" && url === "/api/books") {
+    return db.query("SELECT * FROM books").then(({rows}) => {
+      response.setHeader("content-type", "application/json");
+      response.statusCode = 200;
+      response.write(JSON.stringify({ books: rows }));
+      response.end();
+    })
+  }
+})
 server.listen(8000, (err) => {
   if (err) {
     console.log(err);
